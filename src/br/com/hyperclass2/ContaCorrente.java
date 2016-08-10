700f5c4aa79fe8ee0825eb96ceb0989b02fc6dad
João Carlos Batista <joao_nad@hotmail.com>
2016-08-10 11:39:57 -0300

    Modificando a classe conta corrente. Adicionado o método de evento.

--- src/br/com/hyperclass2/ContaCorrente.java
+++ src/br/com/hyperclass2/ContaCorrente.java
@@ -7,17 +7,20 @@ import java.util.Map;
 public class ContaCorrente {
 	
 	private double saldo;
-	private Map<String, List<EventoTrasacional>> eventoTrasacionals;
+	private final Map<String, List<EventoTrasacional>> eventoTrasacionais;
 	
 	public ContaCorrente(double saldo, Map<String, EventoTrasacional> eventoTrasacionals) {
 		super();
 		this.saldo = saldo;
-		this.eventoTrasacionals = new HashMap<String, List<EventoTrasacional>>();
+		this.eventoTrasacionais = new HashMap<String, List<EventoTrasacional>>();
 	}
 	
-	public void addEventTransaction(String event){
-		if(eventoTrasacionals.containsKey(event)){
-			eventoTrasacionals.put(event, null);
+	public void addEventTransaction(String key, EventoTrasacional eventoTrasacional){
+		
+		if(eventoTrasacionais.containsKey(key)){
+			List<EventoTrasacional> eventoTrasacionaisList = eventoTrasacionais.get(key);
+			eventoTrasacionaisList.addAll(eventoTrasacionaisList);
+			eventoTrasacionais.put(key, eventoTrasacionaisList);
 		}
 	}
 	
